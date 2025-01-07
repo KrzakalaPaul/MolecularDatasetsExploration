77761cf12be04d94c2fadac04ba90827978a5c3c
PaulKrzakala <paul.krzakala@gmail.com>
2025-01-07 19:57:46 +0100

    PUBCHEM Finally working with CPU parrallelization for rdkit

--- data/PUBCHEM_download.sh
+++ data/PUBCHEM_download.sh
@@ -6,4 +6,4 @@ python ./scripts/download_PUBCHEM.py --chunk_size 1000000
 
 # Use RDKit to: 1) Remove invalid smiles, 2) Split the dataset, 3) Convert smiles to graphs
 echo "Processing smiles with RDKit..."
-python ./utils/smiles_to_graphs_bigdata.py --dataset_name PUBCHEM --split_percentage 0.9 0.1 --n_threads 1
+python ./utils/smiles_to_graphs_bigdata.py --dataset_name PUBCHEM --split_percentage 0.9 0.1 --n_threads 8
--- data/utils/smiles_to_graphs_bigdata.py
+++ data/utils/smiles_to_graphs_bigdata.py
@@ -8,27 +8,25 @@ from tqdm import tqdm
 from multiprocessing import Pool
 import lmdb
 
-def format_db(env, n_chunks):
-    i = 0
+def add_size_to_db(env, size):
     with env.begin(write=True) as txn:
-        for chunk_idx in range(n_chunks):
-            j = 0
-            key = f"{chunk_idx}/{j}".encode("ascii")
-            value = txn.get(key)
+        txn.put("size".encode("ascii"), str(size).encode("ascii"))
+        
+def get_size_from_db(env):
+    with env.begin() as txn:
+        return int(txn.get("size".encode("ascii")).decode("ascii"))
+
+def concat_db(env, env_to_concat):
+    previous_size = get_size_from_db(env)
+    with env.begin(write=True) as txn1:
+        size_to_add = get_size_from_db(env_to_concat)
+        with env_to_concat.begin(write=True) as txn2:
+            for i in range(size_to_add):
+                data = txn2.get(f"{i}".encode("ascii"))
+                txn1.put(f"{previous_size+i}".encode("ascii"), data)
+    add_size_to_db(env, previous_size + size_to_add)
             
-            while value is not None:
-                
-                new_key = f"{i}".encode("ascii")
-                txn.put(new_key, value)
-                txn.delete(key)
-                
-                i += 1
-                j += 1
-                key = f"{chunk_idx}/{j}".encode("ascii")
-                value = txn.get(key)
-        txn.put("size".encode("ascii"), str(i).encode("ascii"))
-            
-def add_smiles_list(env_train, env_valid, smiles_list,train_percentage,valid_percentage,chunk_idx):
+def add_smiles_list(env_train, env_valid, smiles_list,train_percentage,valid_percentage):
     
     n_invalid = 0
     n_train = 0
@@ -50,13 +48,16 @@ def add_smiles_list(env_train, env_valid, smiles_list,train_percentage,valid_per
                 data = pickle.dumps(graph)
                 split = random.choice(["train", "valid"], p=[train_percentage, valid_percentage])
                 if split == "train":
-                    key = f"{chunk_idx}/{n_train}".encode("ascii")
+                    key = f"{n_train}".encode("ascii")
                     txn_train.put(key, data)
                     n_train += 1
                 else:
-                    key = f"{chunk_idx}/{n_valid}".encode("ascii")
+                    key = f"{n_valid}".encode("ascii")
                     txn_valid.put(key, data)
                     n_valid += 1
+        
+    add_size_to_db(env_train, n_train)
+    add_size_to_db(env_valid, n_valid)
     
     return n_invalid, n_train, n_valid
 
@@ -86,14 +87,10 @@ if __name__ == "__main__":
     train_percentage, valid_percentage = args.split_percentage
     n_threads = args.n_threads  
     
-    path = f"graphs/{dataset_name}"
-    # Create the database
-    env_train = open_db(path, "train")
-    env_valid = open_db(path, "valid")
-        
     smiles_path = f"smiles/{dataset_name}"
     smiles_files = os.listdir(smiles_path)
 
+    path = f"graphs/{dataset_name}"
     if n_threads == 1:
         
         n_invalid_total = 0
@@ -103,28 +100,37 @@ if __name__ == "__main__":
         for idx in tqdm(range(len(smiles_files))):
             file = smiles_files[idx]
             smiles_list = pd.read_csv(os.path.join(smiles_path, file)).values[:, 0]
-            n_invalid, n_train, n_valid = add_smiles_list(env_train, env_valid, smiles_list, train_percentage=train_percentage, valid_percentage=valid_percentage, chunk_idx=idx)
+
+            env_train = open_db(path, f"train_{idx}")
+            env_valid = open_db(path, f"valid_{idx}")
+            
+            n_invalid, n_train, n_valid = add_smiles_list(env_train, env_valid, smiles_list, train_percentage=train_percentage, valid_percentage=valid_percentage)
             n_invalid_total += n_invalid
             n_train_total += n_train
             n_valid_total += n_valid
+            
+            env_train.close()
+            env_valid.close()
     else:
         
         def process_file(idx):
             file = smiles_files[idx]
             smiles_list = pd.read_csv(os.path.join(smiles_path, file)).values[:, 0]
+            env_train = open_db(path, f"train_{idx}")
+            env_valid = open_db(path, f"valid_{idx}")
             result = add_smiles_list(
                 env_train, env_valid, 
                 smiles_list, 
                 train_percentage=train_percentage, 
-                valid_percentage=valid_percentage, 
-                chunk_idx=idx
+                valid_percentage=valid_percentage
             )
+            env_train.close()
+            env_valid.close()
             return result  
             
         with Pool(n_threads) as p:
             results = list(tqdm(p.imap(process_file, range(len(smiles_files))), total=len(smiles_files)))
 
-            
         n_invalid_total = sum([r[0] for r in results])
         n_train_total = sum([r[1] for r in results])
         n_valid_total = sum([r[2] for r in results])
@@ -133,8 +139,21 @@ if __name__ == "__main__":
     print(f"Total number of training graphs: {n_train_total}")
     print(f"Total number of validation graphs: {n_valid_total}")
     
+    # Concatenate all the chunks
+    env_train = open_db(path, "train")
+    add_size_to_db(env_train, 0)
+    env_valid = open_db(path, "valid")
+    add_size_to_db(env_valid, 0)
+        
+    previous_size = int(env_train.begin().get("size".encode("ascii")).decode("ascii"))
     
-    format_db(env_train, n_chunks=len(smiles_files))
-    format_db(env_valid, n_chunks=len(smiles_files))
-    
-    
+    for idx in range(len(smiles_files)):
+        env_train_chunk = open_db(path, f"train_{idx}", delete=False)
+        concat_db(env_train, env_train_chunk)
+        env_train_chunk.close()
+        os.remove(f"{path}/train_{idx}.lmdb")
+        
+        env_valid_chunk = open_db(path, f"valid_{idx}", delete=False)
+        concat_db(env_valid, env_valid_chunk)
+        env_valid_chunk.close()
+        os.remove(f"{path}/valid_{idx}.lmdb")
