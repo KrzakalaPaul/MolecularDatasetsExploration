1936b7bdfcb71f8628d53f4276389207f0be640b
KrzakalaPaul <paul.krazkala@gmail.com>
2025-02-04 12:41:42 +0100

    add a ALL_download for remote

---
+++ data/ALL_download_slurm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#SBATCH --output=logs/job%j.log
+#SBATCH --error=logs/job%j.err
+#SBATCH --time=24:00:00
+#SBATCH --partition=CPU
+
+set -x
+sh BACE_download.sh
+sh ESOL_download.sh
+sh HIV_download.sh
+sh PCBA_download.sh
+sh PUBCHEM_download.sh
+Sh QM9_download.sh
+sh QM4D_download.sh
+sh TOX21_download.sh
+sh ZINC1M_download.sh
+sh ZINC10M_download.sh
+sh ZINC25k_download.sh
+sh ALCHEMY_download.sh
